c78303aac59166c52397ee008a3c50926973287a
cjlopezp <juqui1978@gmail.com>
2022-11-22 20:52:57 +0100

    22112022 noche

--- js/plus-minus-button.js
+++ js/plus-minus-button.js
@@ -1,12 +1,39 @@
 export let renderPlusMinusButton = () => {
     
-    let quantityItem = document.getElementById("quantityItem");
-    let quantityMinus = document.getElementById("quantityMinus");
-    let quantityPlus = document.getElementById("quantityPlus");
+    let inputField = document.querySelector('.quantity-item input'); 
+    let buttonMinus = document.querySelector('.quantity-minos button');
+    let buttonPlus = document.querySelector('.quantity-plus button');
 
-    quantityPlus.addEventListener("click", () => {
+    buttonPlus.addEventListener("click", () => {      
+        //alert("hola");
+        inputField.value = (parseInt(inputField.value) + 1);
+    });
 
-        // Los datos cuando programamos pueden ser de diferentes tipos, por ejemplo
+    buttonMinus.addEventListener("click", () => {
+       //alert("hola");
+        inputField.value = (parseInt(inputField.value) - 1);       
+    });
+
+
+
+    inputField.addEventListener('keyup', () =>{
+        if (inputField.value == 0 || inputField.value < 0) 
+            buttonMinus.disabled = true
+        else buttonMinus.disabled = false;  
+        });
+
+}
+
+
+
+// inputField.addEventListener("change", function (event) {
+//     if (this.value < 1) this.value = 1; // minimum is 1
+//     else this.value = Math.floor(this.value); // only integers allowed
+// })
+
+
+//querySelec
+// Los datos cuando programamos pueden ser de diferentes tipos, por ejemplo
         // un string (texto), un number (números), un boolean (un 1 o un 0),
         // un array (un conjunto de datos), un objeto, etc.  
 
@@ -16,12 +43,4 @@ export let renderPlusMinusButton = () => {
 
         // Por último, podemos dar un nuevo valor al atributo value del input
         // "age", escribiendo después del igual el nuevo valor que queramos que tenga.
-        //alert("hola");
-        quantityItem.value = (parseInt(quantityItem) + 1);
-    });
-
-    quantityMinus.addEventListener("click", () => {
-       //alert("hola2");
-        quantityItem.value = (parseInt(quantityItem) - 1);
-    });
-}
+        
