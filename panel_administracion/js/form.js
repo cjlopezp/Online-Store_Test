7cfff0238904dded6c59089a52fb5b5075c90085
cjlopezp <juqui1978@gmail.com>
2023-01-09 15:56:08 +0100

    09012023

--- panel_administracion/js/form.js
+++ panel_administracion/js/form.js
@@ -2,66 +2,71 @@ import {validateForm} from './validate.js';
 
 export let renderForm = () => {
 
-    let form = document.querySelector('form');
     let sendFormButton = document.querySelector('.save-button');
 
-    sendFormButton.addEventListener('click', event => {
+    if(sendFormButton){
 
-        event.preventDefault();
+        sendFormButton.addEventListener('click', event => {
 
-        if(!validateForm(form.elements)){
-            return;
-        }
+            event.preventDefault();
 
-        // Podemos recoger el valor de todos los inputs de un formulario mediante el objeto FormData. 
-        // Para ello debemos pasar como parámetro el formulario al que queremos acceder. 
-        // let formData = new FormData(form);
-        // Podemos añadir un nuevo dato al objeto FormData mediante el método append.
-        // formData.append("fingerprint", "123456789");
-
-        // Podemos recorrer el objeto FormData mediante un bucle for...of.
-        // for (let pair of formData.entries()) {
-        //     console.log(pair[0]+ ', ' + pair[1]); 
-        // }
-
-        // Capturar los datos del formulario y enviarlos mediante fetch para ser recibidos por express.
-
-        // Podemos convertir el objeto FormData en un objeto json mediante el método entries.
-
-        let formData = new FormData(form);
-        let formDataJson = Object.fromEntries(formData.entries());
-        let href = window.location.href;
-        let endpointSplit = href.split('/')
-        let endpoint = endpointSplit[endpointSplit.length - 1].split('.')[0];
-        let url = 'http://192.168.1.16:8080/api/admin/' + endpoint;
-
-        // // console.log(JSON.stringify(formDataJson));
-
-        let sendPostRequest = async () => {
-            
-            let request = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-access-token': sessionStorage.getItem('accessToken')
-                },
-                body: JSON.stringify(formDataJson)
-            }).then(response => {
-                return response.json();
-            }).then(data => {
+            let form = document.querySelector('form');
+    
+            if(!validateForm(form.elements)){
+                return;
+            }
+    
+            // Podemos recoger el valor de todos los inputs de un formulario mediante el objeto FormData. 
+            // Para ello debemos pasar como parámetro el formulario al que queremos acceder. 
+            // let formData = new FormData(form);
+            // Podemos añadir un nuevo dato al objeto FormData mediante el método append.
+            // formData.append("fingerprint", "123456789");
+    
+            // Podemos recorrer el objeto FormData mediante un bucle for...of.
+            // for (let pair of formData.entries()) {
+            //     console.log(pair[0]+ ', ' + pair[1]); 
+            // }
+    
+            // Capturar los datos del formulario y enviarlos mediante fetch para ser recibidos por express.
+    
+            // Podemos convertir el objeto FormData en un objeto json mediante el método entries.
+    
+            let formData = new FormData(form);
+            let formDataJson = Object.fromEntries(formData.entries());
+            let href = window.location.href;
+            let endpointSplit = href.split('/')
+            let endpoint = endpointSplit[endpointSplit.length - 1].split('.')[0];
+            let url = 'http://127.0.0.1:8080/api/admin/' + endpoint;
+    
+            // // console.log(JSON.stringify(formDataJson));
+    
+            let sendPostRequest = async () => {
                 
-                document.dispatchEvent(new CustomEvent('message', {
-                    detail: {
-                        text: 'Formulario enviado correctamente',
-                        type: 'success'
-                    }
-                }));
-        
-            }).catch(error => {
-                console.log(error);
-            });
-        }
-
-        sendPostRequest();
-    });
+                let request = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': 'Bearer ' + sessionStorage.getItem('accessToken'),
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formDataJson)
+                }).then(response => {
+                    return response.json();
+                }).then(data => {
+                    
+                    document.dispatchEvent(new CustomEvent('message', {
+                        detail: {
+                            text: 'Formulario enviado correctamente',
+                            type: 'success'
+                        }
+                    }));
+            
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+    
+            sendPostRequest();
+        });
+    }
+    
 };
